1ecbfc263f27ea6c642056884100d7561169f316
sagna <Acer@sagna.com>
2023-01-16 18:30:41 +0000

    'ajout'

---
+++ src/main/java/com/spring/SagnaSpringBoot_AngularApp/SagnaSpringBootAngularAppApplication.java
@@ -0,0 +1,14 @@
+package com.spring.SagnaSpringBoot_AngularApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagnaSpringBootAngularAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagnaSpringBootAngularAppApplication.class, args);
+		System.out.println("helllo");
+	}
+
+}
---
+++ src/test/java/com/spring/SagnaSpringBoot_AngularApp/SagnaSpringBootAngularAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SagnaSpringBoot_AngularApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagnaSpringBootAngularAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
